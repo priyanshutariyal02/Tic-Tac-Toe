0bb7e1b64dde66f534ad6f9e47fa1440533c7150
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-01-30 06:40:06 +0530

    Complete

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing((editing) => !isEditing);
+    }
+
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+        //defaultValue: just set an initial value instead of enforcing a value thereafter.
+        // value: overwrites any changes we're trying to do.
+    }
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
