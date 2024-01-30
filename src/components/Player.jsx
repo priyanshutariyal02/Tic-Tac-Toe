f5011763c15026e0163c67c3fe9fa1e387f89752
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-01-30 06:32:49 +0530

    Edit and save player name functionality is finish

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from 'react'
 
-const Player = ({ name, symbol }) => {
+const Player = ({ initialName, symbol }) => {
+    const [playerName, setPlayerName] = useState(initialName);
 
     const [isEditing, setIsEditing] = useState(false);
+
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing((editing) => !isEditing);
     }
-    let playerName = <span className="player-name">{name}</span>
+
+    function handleChange(event) {
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>
 
     if (isEditing) {
-        playerName = <input type="text" required autoFocus />
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+        //defaultValue: just set an initial value instead of enforcing a value thereafter.
+        // value: overwrites any changes we're trying to do.
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
