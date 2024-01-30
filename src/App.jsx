0495aa816c0571a01e604b541dc12420606c9355
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-01-30 08:58:38 +0530

    Create new component GameBoard.jsx and modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import GameBoard from "./components/Gameboard.jsx"
 import Player from "./components/Player.jsx"
-
+import { useState } from "react";
 function App() {
+  const [playerActive, setPlayerActive] = useState('X');
+
+  function handleSelectSquare() {
+    setPlayerActive((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={playerActive === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={playerActive === 'O'} />
         </ol>
-        GAME BOEARD
+
+        {/* GAME BOEARD */}
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol ={playerActive} />
       </div>
       LOG
     </main>
-  )
+  ) 
 }
 
 export default App
