322914b190eb0a4b88a210fc3b431103101e4623
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-02-10 22:31:52 +0530

    Modify App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,55 @@
+import React, { useState } from "react";
 import GameBoard from "./components/Gameboard.jsx"
+import Log from "./components/Log.jsx";
 import Player from "./components/Player.jsx"
-import { useState } from "react";
-function App() {
-  const [playerActive, setPlayerActive] = useState('X');
 
-  function handleSelectSquare() {
-    setPlayerActive((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+//helper function
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
+
+function App() {
+
+  const [gameTurns, setGameTurns] = useState([]);
+
+  // const [playerActive, setPlayerActive] = useState('X');
+  const activePalyer = deriveActivePlayer(gameTurns);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setPlayerActive((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+
+    setGameTurns((prevTurns) => {
+
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns,];
+
+      return updatedTurns;
+    });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={playerActive === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={playerActive === 'O'} />
+          <Player initialName="Player 1" symbol="X" isActive={activePalyer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePalyer === 'O'} />
         </ol>
 
         {/* GAME BOEARD */}
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol ={playerActive} />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns} />
       </div>
-      LOG
+      {/* LOG */}
+      <Log turns={gameTurns} />
     </main>
-  ) 
+  )
 }
 
 export default App
