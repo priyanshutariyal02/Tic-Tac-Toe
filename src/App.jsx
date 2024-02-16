efbe70eb6bb443e4727e523f463fc2f31dd0aee0
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-02-16 21:43:37 +0530

    TIC-TAC-TOE Game Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import React, { useState } from "react";
 import GameBoard from "./components/Gameboard.jsx"
 import Log from "./components/Log.jsx";
 import Player from "./components/Player.jsx"
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+import GameOver from "./components/GameOver.jsx";
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2',
+}
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 
 //helper function
 function deriveActivePlayer(gameTurns) {
@@ -13,13 +27,49 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
+function deriveGameBoard(gameTurns) {
+  // let gameBoard = initialGameBoard; bug
+
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])]; // creating the copy of initial array to fix bug & it must be a deep copy
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+    }
+  }
+  return winner
+}
 
 function App() {
 
-  const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState(PLAYERS);
 
-  // const [playerActive, setPlayerActive] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
   const activePalyer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setPlayerActive((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
@@ -33,22 +83,47 @@ function App() {
       return updatedTurns;
     });
   }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPalyers => {
+      return {
+        ...prevPalyers,
+        [symbol]: newName,
+      }
+    })
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePalyer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePalyer === 'O'} />
+          <Player
+            initialName={PLAYERS.X}
+            symbol="X"
+            isActive={activePalyer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName={PLAYERS.O}
+            symbol="O"
+            isActive={activePalyer === 'O'}
+          />
         </ol>
+
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={() => handleRestart()} />}
 
         {/* GAME BOEARD */}
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          turns={gameTurns} />
+          board={gameBoard} />
       </div>
       {/* LOG */}
       <Log turns={gameTurns} />
-    </main>
+    </main >
   )
 }
 
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
-const Player = ({ initialName, symbol, isActive }) => {
+const Player = ({ initialName, symbol, isActive, onChangeName }) => {
     const [playerName, setPlayerName] = useState(initialName);
 
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !isEditing);
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
